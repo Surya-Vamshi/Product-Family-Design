b95ac24467d7970e00f478be399fd8c97771cc03
Surya Vamshi Penumarthi <p.suryavamshi5@gmail.com>
2022-06-17 18:59:11 +0200

    update: gui_main.py (added change_prod function)

--- _04_Solution_Spaces/_100_design_space_projection/gui_main.py
+++ _04_Solution_Spaces/_100_design_space_projection/gui_main.py
@@ -25,14 +25,18 @@ class gui_main(QDialog):
 
         # Importing problem x-ray file
         module = importlib.import_module("_03_Design_Problems." + problem)
-        problem = getattr(module, problem)
-        self.p = problem()
-        self.sample_size = self.p.sampleSize
+        self.problem = getattr(module, problem)
+        self.p = []
+        self.p.append(self.problem())
+        self.currentProdNum = 0
+        self.currentProdName = ["Product 1"]
 
         # window title, icon and geometry
         screensize = QGuiApplication.primaryScreen().availableSize()
         width = screensize.width()
         height = screensize.height() - 40
+        font = QFont()
+        font.setPointSize(10)
         self.setGeometry(500, 200, 0.33 * width, height)
         # self.setFixedSize(0.33*width, height)
         self.move(QPoint(0.67 * width, 0))
@@ -51,15 +55,29 @@ class gui_main(QDialog):
 
         self.label_sample_size = QLabel("Sample Size:", self)
         self.label_sample_size.setGeometry(QRect(420, 20, 120, 30))
-        font = QFont()
-        font.setPointSize(10)
         self.label_sample_size.setFont(font)
 
-        self.sample_size_input = QLineEdit(str(self.sample_size), self)
+        self.sample_size_input = QLineEdit(str(self.p[self.currentProdNum].sampleSize), self)
         self.onlyInt = QIntValidator()
         self.onlyDouble = QDoubleValidator()
         self.sample_size_input.setValidator(self.onlyInt)
         self.sample_size_input.setGeometry(QRect(500, 20, 120, 30))
+
+        # Refresh Tab Window call
+        self.init_product_data()
+
+        print("GUI is called " + str(problem))
+        print("D = ", self.p[self.currentProdNum].d)
+        print("M = ", self.p[self.currentProdNum].m)
+        print("NP = ", self.p[self.currentProdNum].np)
+
+        self.show()
+
+    def init_product_data(self):
+        screensize = QGuiApplication.primaryScreen().availableSize()
+        width = screensize.width()
+        # Sample Size input update
+        self.sample_size_input.setText(str(self.p[self.currentProdNum].sampleSize))
 
         # Main Tab Windows
         self.tabWidget = QTabWidget(self)
@@ -69,6 +87,8 @@ class gui_main(QDialog):
         self.tabWidget.addTab(self.DV, "Design Variables")
         self.QOI_P = QWidget()
         self.tabWidget.addTab(self.QOI_P, "Quantities of Interest and Parameters")
+        font = QFont()
+        font.setPointSize(10)
         self.tabWidget.setFont(font)
 
         # Design Variables Tab
@@ -105,27 +125,27 @@ class gui_main(QDialog):
         self.DV_Grid.addWidget(DV_Header5, 0, 5, 1, 1)
 
         # Generating a table with design variables as per X-ray file
-        x_size = len(self.p.x)
+        x_size = len(self.p[self.currentProdNum].x)
         for i in range(0, x_size):
-            DV_name = QLabel(self.p.x[i]["name"])
+            DV_name = QLabel(self.p[self.currentProdNum].x[i]["name"])
             DV_name.setFixedWidth(DV_Grid_Width * 0.175)
             self.DV_Grid.addWidget(DV_name, 2 * i + 1, 0, 2, 1)
-            setattr(self.DV_Grid, self.p.x[i]["name"], DV_name)
+            setattr(self.DV_Grid, "name" + str(i), DV_name)
 
         for i in range(0, x_size):
-            DV_unit = QLabel(self.p.x[i]["unit"])
+            DV_unit = QLabel(self.p[self.currentProdNum].x[i]["unit"])
             DV_unit.setFixedWidth(DV_Grid_Width * 0.075)
             self.DV_Grid.addWidget(DV_unit, 2 * i + 1, 1, 2, 1)
             setattr(self.DV_Grid, "unit" + str(i), DV_unit)
 
         for i in range(0, x_size):
-            DV_dsl = QLineEdit(str(self.p.x[i]["dsl"]))
+            DV_dsl = QLineEdit(str(self.p[self.currentProdNum].x[i]["dsl"]))
             DV_dsl.setMaximumWidth(DV_Grid_Width * 0.1)
             self.DV_Grid.addWidget(DV_dsl, 2 * i + 1, 2, 2, 1)
             setattr(self.DV_Grid, "dsl" + str(i), DV_dsl)
 
         for i in range(0, x_size):
-            DV_l = QLineEdit(str(self.p.x[i]["l"]))
+            DV_l = QLineEdit(str(self.p[self.currentProdNum].x[i]["l"]))
             DV_l.setMaximumWidth(DV_Grid_Width * 0.15)
             self.DV_Grid.addWidget(DV_l, 2 * i + 1, 3, 1, 1)
             setattr(self.DV_Grid, "l" + str(i), DV_l)
@@ -139,19 +159,19 @@ class gui_main(QDialog):
             setattr(self.DV_Grid, "range" + str(i), DV_range)
 
         for i in range(0, x_size):
-            DV_u = QLineEdit(str(self.p.x[i]["u"]))
+            DV_u = QLineEdit(str(self.p[self.currentProdNum].x[i]["u"]))
             DV_u.setMaximumWidth(DV_Grid_Width * 0.15)
             self.DV_Grid.addWidget(DV_u, 2 * i + 1, 4, 1, 1)
             setattr(self.DV_Grid, "u" + str(i), DV_u)
 
         for i in range(0, x_size):
-            DV_dsu = QLineEdit(str(self.p.x[i]["dsu"]))
+            DV_dsu = QLineEdit(str(self.p[self.currentProdNum].x[i]["dsu"]))
             DV_dsu.setMaximumWidth(DV_Grid_Width * 0.1)
             self.DV_Grid.addWidget(DV_dsu, 2 * i + 1, 5, 2, 1)
             setattr(self.DV_Grid, "dsu" + str(i), DV_dsu)
 
-        test = getattr(self.DV_Grid, self.p.x[0]["name"])
-        test.setText("Z_2222")
+        # test = getattr(self.DV_Grid, self.p[self.currentProdNum].x[0]["name"])
+        # test.setText("Z_2222")
 
         # Setting dimensions for the Design Variable table
         for i in range(0, x_size):
@@ -202,7 +222,7 @@ class gui_main(QDialog):
         self.QOI_P_Grid1.addWidget(QOI_P_Header7, 0, 6, 1, 1)
 
         # Generating a table with Quantities of Interest as per X-ray file
-        y_size = len(self.p.y)
+        y_size = len(self.p[self.currentProdNum].y)
 
         for i in range(0, y_size):
             QOI_P_visible = QCheckBox()
@@ -212,37 +232,37 @@ class gui_main(QDialog):
 
         for i in range(0, y_size):
             QOI_P_active = QCheckBox()
-            if self.p.y[i]["active"] == 1:
+            if self.p[self.currentProdNum].y[i]["active"] == 1:
                 QOI_P_active.setChecked(True)
             self.QOI_P_Grid1.addWidget(QOI_P_active, i + 1, 1, 1, 1)
             setattr(self.QOI_P_Grid1, "active" + str(i), QOI_P_active)
 
         for i in range(0, y_size):
-            QOI_P_name = QLabel(self.p.y[i]["name"])
+            QOI_P_name = QLabel(self.p[self.currentProdNum].y[i]["name"])
             QOI_P_name.setFixedWidth(QOI_P_Grid_Width * 0.175)
             self.QOI_P_Grid1.addWidget(QOI_P_name, i + 1, 2, 1, 1)
-            setattr(self.QOI_P_Grid1, self.p.y[i]["name"], QOI_P_name)
+            setattr(self.QOI_P_Grid1, self.p[self.currentProdNum].y[i]["name"], QOI_P_name)
 
         for i in range(0, y_size):
-            QOI_P_unit = QLabel(self.p.y[i]["unit"])
+            QOI_P_unit = QLabel(self.p[self.currentProdNum].y[i]["unit"])
             QOI_P_unit.setFixedWidth(QOI_P_Grid_Width * 0.075)
             self.QOI_P_Grid1.addWidget(QOI_P_unit, i + 1, 3, 1, 1)
             setattr(self.QOI_P_Grid1, "unit" + str(i), QOI_P_unit)
 
         for i in range(0, y_size):
-            QOI_P_l = QLineEdit(str(self.p.y[i]["l"]))
+            QOI_P_l = QLineEdit(str(self.p[self.currentProdNum].y[i]["l"]))
             QOI_P_l.setMaximumWidth(QOI_P_Grid_Width * 0.15)
             self.QOI_P_Grid1.addWidget(QOI_P_l, i + 1, 4, 1, 1)
             setattr(self.QOI_P_Grid1, "l" + str(i), QOI_P_l)
 
         for i in range(0, y_size):
-            QOI_P_u = QLineEdit(str(self.p.y[i]["u"]))
+            QOI_P_u = QLineEdit(str(self.p[self.currentProdNum].y[i]["u"]))
             QOI_P_u.setMaximumWidth(QOI_P_Grid_Width * 0.15)
             self.QOI_P_Grid1.addWidget(QOI_P_u, i + 1, 5, 1, 1)
             setattr(self.QOI_P_Grid1, "u" + str(i), QOI_P_u)
 
         for i in range(0, y_size):
-            [r, b, g] = self.p.y[i]["color"]
+            [r, b, g] = self.p[self.currentProdNum].y[i]["color"]
             text = "#%02x%02x%02x" % (r, g, b)
             QOI_P_colorbtn = QPushButton(text)
             QOI_P_colorbtn.setStyleSheet("background-color: #%02x%02x%02x" % (r, g, b))
@@ -275,22 +295,22 @@ class gui_main(QDialog):
         self.QOI_P_Grid2.addWidget(QOI_P_Header3, 0, 2, 1, 1)
 
         # Generating a table with Parameters as per X-ray file
-        para_size = len(self.p.p)
+        para_size = len(self.p[self.currentProdNum].p)
 
         for i in range(0, para_size):
-            QOI_P_name = QLabel(self.p.p[i]["name"])
+            QOI_P_name = QLabel(self.p[self.currentProdNum].p[i]["name"])
             QOI_P_name.setFixedWidth(QOI_P_Grid_Width * 0.35)
             self.QOI_P_Grid2.addWidget(QOI_P_name, i + 1, 0, 1, 1)
-            setattr(self.QOI_P_Grid2, self.p.p[i]["name"], QOI_P_name)
+            setattr(self.QOI_P_Grid2, self.p[self.currentProdNum].p[i]["name"], QOI_P_name)
 
         for i in range(0, para_size):
-            QOI_P_unit = QLabel(self.p.p[i]["unit"])
+            QOI_P_unit = QLabel(self.p[self.currentProdNum].p[i]["unit"])
             QOI_P_unit.setFixedWidth(QOI_P_Grid_Width * 0.15)
             self.QOI_P_Grid2.addWidget(QOI_P_unit, i + 1, 1, 1, 1)
             setattr(self.QOI_P_Grid2, "unit" + str(i), QOI_P_unit)
 
         for i in range(0, para_size):
-            QOI_P_value = QLineEdit(str(self.p.p[i]["value"]))
+            QOI_P_value = QLineEdit(str(self.p[self.currentProdNum].p[i]["value"]))
             QOI_P_value.setMaximumWidth(DV_Grid_Width * 0.5)
             self.QOI_P_Grid2.addWidget(QOI_P_value, i + 1, 2, 1, 1)
             setattr(self.QOI_P_Grid2, "value" + str(i), QOI_P_value)
@@ -303,23 +323,67 @@ class gui_main(QDialog):
         self.QOI_P_toolboxmain.addItem(self.QOI_P_toolbox1,
                                        "Select the Quantities of Interest values as per requirement:")
         self.QOI_P_toolboxmain.addItem(self.QOI_P_toolbox2, "Select the Parameters values as per requirement:")
-
-        print("GUI is called " + str(problem))
-        print("D = ", self.p.d)
-        print("M = ", self.p.m)
-        print("NP = ", self.p.np)
-
         self.show()
 
     def update_values(self):
-        # self.sample_size = self.sample_size_input.text()
-        # print(self.sample_size)
+        self.p[self.currentProdNum].sampleSize = self.sample_size_input.text()
+        print(self.p[self.currentProdNum].sampleSize)
         # test = getattr(self.DV_Grid, "u1")
         # print(test.text())
         # test = getattr(self.QOI_P_Grid1, "color0")
         # print(test)
-        print(self.printingtest)
-        # print(self.p.y[1]["color"])
+        # print(self.printingtest)
+        # print(self.p[self.currentProdNum].y[1]["color"])
+        self.p.append(self.problem())
+        self.currentProdName.append("Product 2")
+        self.currentProdNum = 1
+        self.p[1].x[0]["dsl"] = 500
+        self.p[1].x[1]["u"] = 2000
+
+        self.change_prod()
+
+    def change_prod(self):
+        # Changing Design Variable values
+        for i in range(0, len(self.p[self.currentProdNum].x)):
+            DV_dsl = getattr(self.DV_Grid, "dsl" + str(i))
+            DV_dsl.setText(str(self.p[self.currentProdNum].x[i]["dsl"]))
+
+            DV_l = getattr(self.DV_Grid, "l" + str(i))
+            DV_l.setText(str(self.p[self.currentProdNum].x[i]["l"]))
+
+            # Need to Add Ranger values later
+
+            DV_u = getattr(self.DV_Grid, "u" + str(i))
+            DV_u.setText(str(self.p[self.currentProdNum].x[i]["u"]))
+
+            DV_dsu = getattr(self.DV_Grid, "dsu" + str(i))
+            DV_dsu.setText(str(self.p[self.currentProdNum].x[i]["dsu"]))
+
+        # Changing Quantities of Interest Values
+        for i in range(0, len(self.p[self.currentProdNum].y)):
+            QOI_P_visible = getattr(self.QOI_P_Grid1, "visible" + str(i))
+            QOI_P_visible.setChecked(True)
+
+            QOI_P_active = getattr(self.QOI_P_Grid1, "active" + str(i))
+            if self.p[self.currentProdNum].y[i]["active"] == 1:
+                QOI_P_active.setChecked(True)
+            else:
+                QOI_P_active.setChecked(False)
+
+            QOI_P_l = getattr(self.QOI_P_Grid1, "l" + str(i))
+            QOI_P_l.setText(str(self.p[self.currentProdNum].y[i]["l"]))
+
+            QOI_P_u = getattr(self.QOI_P_Grid1, "u" + str(i))
+            QOI_P_u.setText(str(self.p[self.currentProdNum].y[i]["u"]))
+
+            [r, b, g] = self.p[self.currentProdNum].y[i]["color"]
+            QOI_P_colorbtn = getattr(self.QOI_P_Grid1, "color" + str(i))
+            QOI_P_colorbtn.setStyleSheet("background-color: #%02x%02x%02x" % (r, g, b))
+
+        # Changing Parameters Values
+        for i in range(0, len(self.p[self.currentProdNum].p)):
+            QOI_P_value = getattr(self.QOI_P_Grid2, "value" + str(i))
+            QOI_P_value.setText(str(self.p[self.currentProdNum].p[i]["value"]))
 
     def run_gui_main(self):
         print("Need to call SolutionSpace")
@@ -330,20 +394,19 @@ class gui_main(QDialog):
         '''
         dlg = QColorDialog(self)
         dlg.exec()
-        # dlg = QColorDialog(self)
+
         # if self._color:
         #     dlg.setCurrentColor(QColor(self._color))
-        #
         if dlg.exec():
-            for i in range(0, len(self.p.y)):
+            for i in range(0, len(self.p[self.currentProdNum].y)):
                 QOI_color = getattr(self.QOI_P_Grid1, "color" + str(i))
-                [r, g, b] = self.p.y[i]["color"]
+                [r, g, b] = self.p[self.currentProdNum].y[i]["color"]
                 text = "#%02x%02x%02x" % (r, g, b)
                 if (QOI_color.text() == text):
                     QOI_color.setText(dlg.currentColor().name())
                     h = dlg.currentColor().name().lstrip("#")
                     (r, g, b) = tuple(int(h[k:k + 2], 16) for k in (0, 2, 4))
-                    self.p.y[i]["color"] = [r, g, b]
+                    self.p[self.currentProdNum].y[i]["color"] = [r, g, b]
                     QOI_color.setStyleSheet("background-color: #%02x%02x%02x" % (r, g, b))
 
 
