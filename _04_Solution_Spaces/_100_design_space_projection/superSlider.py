61ebc3b03146203e9340d993deffd149edd18560
Surya-Vamshi <p.suryavamshi5@gmail.com>
2022-07-05 18:38:00 +0200

    Completed: superSlider.py

--- _04_Solution_Spaces/_100_design_space_projection/superSlider.py
+++ _04_Solution_Spaces/_100_design_space_projection/superSlider.py
@@ -19,7 +19,6 @@ class superSlider(QtWidgets.QSlider):
         self.hover_control = QtWidgets.QStyle.SC_None
         self.click_offset = 0
 
-        # 0 for the low, 1 for the high, -1 for both
         self.active_slider = 0
 
     def pick(self, pt):
@@ -48,40 +47,46 @@ class superSlider(QtWidgets.QSlider):
         return style.sliderValueFromPosition(self.minimum(), self.maximum(), pos - slider_min, slider_max - slider_min,
                                              opt.upsideDown)
 
-    def mousePressed(self, event):
+    def mousePressEvent(self, event):
         event.accept()
 
         style = QtWidgets.QApplication.style()
-        btn = event.button()
+        button = event.button()
 
-        if btn:
+        # In a normal slider control, when the user clicks on a point in the
+        # slider's total range, but not on the slider part of the control the
+        # control would jump the slider value to where the user clicked.
+        # For this control, clicks which are not direct hits will slide both
+        # slider parts
+
+        if button:
             opt = QtWidgets.QStyleOptionSlider()
             self.initStyleOption(opt)
 
-            self.active = -1
+            self.active_slider = -1
 
             for i, value in enumerate([self.l, self.u]):
                 opt.sliderPosition = value
                 hit = style.hitTestComplexControl(style.CC_Slider, opt, event.pos(), self)
                 if hit == style.SC_SliderHandle:
-                    self.active = i
-                    self.pressed = hit
+                    self.active_slider = i
+                    self.pressed_control = hit
 
                     self.triggerAction(self.SliderMove)
                     self.setRepeatAction(self.SliderNoAction)
                     self.setSliderDown(True)
                     break
 
-            if self.active < 0:
-                self.pressed = QtWidgets.QStyle.SC_SliderHandle
+            if self.active_slider < 0:
+                self.pressed_control = QtWidgets.QStyle.SC_SliderHandle
                 self.click_offset = self.pixelPosToRangeValue(self.pick(event.pos()))
                 self.triggerAction(self.SliderMove)
                 self.setRepeatAction(self.SliderNoAction)
         else:
             event.ignore()
 
-    def mouseMoved(self, event):
-        if self.pressed != QtWidgets.QStyle.SC_SliderHandle:
+    def mouseMoveEvent(self, event):
+        if self.pressed_control != QtWidgets.QStyle.SC_SliderHandle:
             event.ignore()
             return
 
@@ -90,7 +95,7 @@ class superSlider(QtWidgets.QSlider):
         opt = QtWidgets.QStyleOptionSlider()
         self.initStyleOption(opt)
 
-        if self.active < 0:
+        if self.active_slider < 0:
             offset = new_pos - self.click_offset
             self.u += offset
             self.l += offset
@@ -115,6 +120,7 @@ class superSlider(QtWidgets.QSlider):
 
         self.update()
 
+        # self.emit(QtCore.SIGNAL('sliderMoved(int)'), new_pos)
         self.sliderMoved.emit(self.l, self.u)
 
     def low(self):
@@ -144,3 +150,56 @@ class superSlider(QtWidgets.QSlider):
             opt.subControls |= QtWidgets.QStyle.SC_SliderTickmarks
         style.drawComplexControl(QtWidgets.QStyle.CC_Slider, opt, painter, self)
         groove = style.subControlRect(QtWidgets.QStyle.CC_Slider, opt, QtWidgets.QStyle.SC_SliderGroove, self)
+
+        self.initStyleOption(opt)
+        opt.subControls = QtWidgets.QStyle.SC_SliderGroove
+
+        opt.siderValue = 0
+        opt.sliderPosition = self.l
+        low_rect = style.subControlRect(QtWidgets.QStyle.CC_Slider, opt, QtWidgets.QStyle.SC_SliderHandle, self)
+        opt.sliderPosition = self.u
+        high_rect = style.subControlRect(QtWidgets.QStyle.CC_Slider, opt, QtWidgets.QStyle.SC_SliderHandle, self)
+
+        low_pos = self.pick(low_rect.center())
+        high_pos = self.pick(high_rect.center())
+
+        min_pos = min(low_pos, high_pos)
+        max_pos = max(low_pos, high_pos)
+
+        c = QtCore.QRect(low_rect.center(), high_rect.center()).center()
+
+        if opt.orientation == QtCore.Qt.Horizontal:
+            span_rect = QtCore.QRect(QtCore.QPoint(min_pos, c.y() - 2), QtCore.QPoint(max_pos, c.y() + 1))
+        else:
+            span_rect = QtCore.QRect(QtCore.QPoint(c.x() - 2, min_pos), QtCore.QPoint(c.x() + 1, max_pos))
+
+        if opt.orientation == QtCore.Qt.Horizontal:
+            groove.adjust(0, 0, -1, 0)
+        else:
+            groove.adjust(0, 0, 0, -1)
+
+        highlight = self.palette().color(QtGui.QPalette.Highlight)
+        painter.setBrush(QtGui.QBrush(highlight))
+        painter.setPen(QtGui.QPen(highlight, 0))
+        painter.drawRect(span_rect.intersected(groove))
+
+        for i, value in enumerate([self.l, self.u]):
+            opt = QtWidgets.QStyleOptionSlider()
+            self.initStyleOption(opt)
+
+            if i == 0:
+                opt.subControls = QtWidgets.QStyle.SC_SliderHandle
+            else:
+                opt.subControls = QtWidgets.QStyle.SC_SliderHandle
+
+            if self.tickPosition() != self.NoTicks:
+                opt.subControls |= QtWidgets.QStyle.SC_SliderTickmarks
+
+            if self.pressed_control:
+                opt.activeSubControls = self.pressed_control
+            else:
+                opt.activeSubControls = self.hover_control
+
+            opt.sliderPosition = value
+            opt.sliderValue = value
+            style.drawComplexControl(QtWidgets.QStyle.CC_Slider, opt, painter, self)
