0f9a884c7d7ca4fa8ce009e6c4bed83a4999193b
Surya-Vamshi <p.suryavamshi5@gmail.com>
2022-07-13 19:53:25 +0200

    Update: max_SolutionSpace_for_fixed_lower_bounds

--- _04_Solution_Spaces/_200_MO-SSE/max_SolutionSpace_for_fixed_lower_bounds.py
+++ _04_Solution_Spaces/_200_MO-SSE/max_SolutionSpace_for_fixed_lower_bounds.py
@@ -25,9 +25,11 @@ def max_SolutionSpace_for_fixed_lower_bounds(problem, dvbox, parameters, mu, g, 
     mu: Size of the solution SPACE
     """
     # Importing Modules
+    import warnings
     import numpy as np
     from _07_Algorithms.Constraint import Constraint
 
+    print("Testing of max_SolutionSpace_for_fixed_lower_bounds:")
 
     # Pre-processing
 
@@ -40,13 +42,13 @@ def max_SolutionSpace_for_fixed_lower_bounds(problem, dvbox, parameters, mu, g, 
 
     # Check which DVs can be independently expanded to higher values
     expandable_dv = np.zeros_like(dsl)
-    ind_dv_in_par_vector = np.argwhere(~np.isnan(parameters[0]))
+    ind_dv_in_par_vector = np.argwhere(np.isnan(parameters[0, :])).squeeze()
     parameters_initial = parameters
 
     # Expand upper bound of every DV and check if the constraints are met
     for i in range(0, dim):
-        x = dvbox[1, :]
-        x[i] = x[i] + dsu[i] - dsl[i] * 0.05
+        x = np.array(dvbox[1, :])
+        x[i] = x[i] + (dsu[i] - dsl[i]) * 0.05
 
         const = Constraint(problem, reqU, reqL, parameters, dv_norm, dv_norm_l, qoi_norm, 1)
         if all(const.Constraint_fun(x) <= 0):
@@ -54,7 +56,110 @@ def max_SolutionSpace_for_fixed_lower_bounds(problem, dvbox, parameters, mu, g, 
         else:
             # Make non expandable DVs to parameters
             parameters[:, ind_dv_in_par_vector[i]] = np.transpose(dvbox[:, i])*dv_norm[i] + dv_norm_l[i]
-            """Need to check if everything is correct"""
+
+    # Checking if no DV is possible to expand to higher values
+    if sum(expandable_dv) == 0:
+        dvbox = dvbox_initial
+        mu = mu_initial
+        return [dvbox, mu]
+
+    # Reduce dvbox, dsl, dsu, dv_norm and weight to just the expandable DVs
+    dvbox = dvbox[:, expandable_dv == 1]
+    dsl = dsl[expandable_dv == 1]
+    dsu = dsu[expandable_dv == 1]
+    dv_norm = dv_norm[expandable_dv == 1]
+    dv_norm_l = dv_norm_l[expandable_dv == 1]
+    weight = weight[expandable_dv == 1]
+    weight = weight / sum(weight)
+
+    dim = len(dsl)
+
+    ind_parameters = np.argwhere(~np.isnan(parameters[0, :])).squeeze()
+
+    """ Phase I for initial Point """
+    dvbox_old = dvbox
+
+    Iter_Phase_I = 0
+    mu_vec = mu
+
+    # At first expand dvbox isotroply in all directions
+    anisotropic_expandation = 0
+    tol_Phase_I = 5e-2
+
+    while True:
+        Iter_StepB = 0
+        while True:
+            Iter_StepB = Iter_StepB + 1
+
+            # Step B
+            if ~anisotropic_expandation:
+                # Expand dvbox in upper sides of each interval isotroply
+                dvbox_new = dvbox_old + g*(dsu - dsl)*[[0], [1]]
+            else:
+                # Expand dvbox in upper sides of each interval anisotroply
+                dvbox_new = dvbox_old + g*(dvbox_old[1, :] - dvbox_old[0, :])*[[0], [1]]
+
+            # Expend maximal to DS bounds
+            dvbox_new[0, :] = np.maximum(dvbox_new[0, :], dsl)
+            dvbox_new[1, :] = np.minimum(dvbox_new[1, :], dsu)
+
+            # Monte Carlo Sampling
+            m = 0
+            """need to add monte carlo sampling"""
+
+            # 50% of the samples in the candidate box have to be good designs to
+            # end the growing process. Otherwise, the growth rate must be set
+            # smaller and the growing reruns. End also with maximal Iterations
+            if m/N >= 0.5 or Iter_StepB >= 100:
+                dvbox = dvbox_new
+
+                # Step size control only for maximal Solution Space.
+                # Otherwise, the points are necessary for pareto front.
+
+                # Change step size to a bigger value if there were enough good
+                # design points in the step
+                g = 2*m/N*g
+                break
+            else:
+                # Change step size to a smaller value if there were not enough
+                # good design points in the step and repeat step
+                g = 2 * m / N * g
+
+                # If m is zero, then g is zero in every following step ==>
+                # further expansion is not possible ==> g has to have at least
+                # a small, nonzero and positive value
+                if g <= 0:
+                    g = 0.006
+
+        if m != 0:
+            Iter_Phase_I = Iter_Phase_I + 1
+
+            # Step A
+            """need to add StepA_modified_upper_bounds"""
+
+            # Stop phase I if mu doesn't change significantly from step to step
+            # or if the maximal iteration is reached
+            if abs(0) < tol_Phase_I:
+                """Need to edit this"""
+                if anisotropic_expandation == 0:
+                    # When isotropic expandation converges, expand anisotropic
+                    # with lower tolerance
+                    anisotropic_expandation = 1
+                    tol_Phase_I = 1e-2
+                else:
+                    break
+            elif Iter_Phase_I > 1000:
+                warnings.warn("Warning! Phase I stopped due to reaching the maximal iteration counter! Solution may "
+                              "not be the maximal Solution Space!")
+                break
+        else:
+            dvbox = dvbox_initial
+            mu = mu_initial
+            return [dvbox, mu]
+        dvbox_old = dvbox
+
+    """Intermediate Phase  for initial Point"""
+    # Monte Carlo Sampling
 
     return [dvbox, mu]
 
